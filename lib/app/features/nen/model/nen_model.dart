3a65e20301274d7adc70897e3937f659a253ac13
João Neto <jagomes694@gmail.com>
2024-02-08 17:30:36 -0300

    algumas formatações feitas

--- lib/app/features/nen/model/nen_model.dart
+++ lib/app/features/nen/model/nen_model.dart
@@ -1,40 +1,13 @@
-import 'dart:convert';
-
 class Nen {
-
   final List titulos;
   final List content;
   final List images;
 
-  Nen({required this.titulos, required this.content, required this.images,});
-
-  Nen copyWith({
-    List? titulos,
-    List? content,
-    List? images,
-  }) {
-    return Nen(
-      titulos: titulos ?? this.titulos,
-      content: content ?? this.content,
-      images: images ?? this.images,
-    );
-  }
-
-
-  static Nen? fromJson(String? source) {
-    if (source?.isEmpty ?? true) return Nen.fromMap({});
-    return Nen.fromMap(json.decode(source!));
-  }
-
-  String toJson() => json.encode(toMap());
-
-  Map<String, dynamic> toMap() {
-    return {
-      'content': content,
-      'images': images,
-      'titulos': titulos,
-    };
-  }
+  Nen({
+    required this.titulos,
+    required this.content,
+    required this.images,
+  });
 
   factory Nen.fromMap(Map<String, dynamic> map) {
     return Nen(
@@ -43,5 +16,4 @@ class Nen {
       titulos: map['titulos'] as List,
     );
   }
-
 }
